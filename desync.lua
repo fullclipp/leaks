25680052e0cbcfbdf675bc27fde9cbb35911b29c
xz <95953670+laagginq@users.noreply.github.com>
2022-07-28 16:27:09 -0700

    Delete desync.lua

--- desync.lua
+++
@@ -1,4 +0,0 @@
--- // Press Z to enable/disable
-hookfunction(game.Players.LocalPlayer.IsInGroup, function() return true end)
-loadstring(game:HttpGet('https://raw.githubusercontent.com/laagginq/storage/main/scripts/desync.lua'))()
--- // I DIDNT MAKE THIS I JUST BYPASSED GROUP WL AND REUPLOADED TO MY GH
